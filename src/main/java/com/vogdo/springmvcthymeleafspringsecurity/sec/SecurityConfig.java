e534dd8878eb07ad5c076fa75ce292283395e600
Mohamed Makhtout <medmakhtout@gmail.com>
2025-08-24 13:11:17 +0100

    new login page look, add more secure functions, etc

---
+++ src/main/java/com/vogdo/springmvcthymeleafspringsecurity/sec/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.vogdo.springmvcthymeleafspringsecurity.sec;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        PasswordEncoder encoder = passwordEncoder();
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user1").password(passwordEncoder().encode("1234")).roles("USER").build(),
+                User.withUsername("user2").password(passwordEncoder().encode("1234")).roles("USER").build(),
+                User.withUsername("admin").password(passwordEncoder().encode("1234")).roles("USER","ADMIN").build()
+        );
+    }
+    //@Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                // a completer
+                return null;
+            }
+        };
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .formLogin(fl->fl.loginPage("/login").permitAll())
+                //.formLogin(Customizer.withDefaults())
+                //.csrf(csrf->csrf.disable()) //it is better to disable csrf when using stateless authentification
+                // cause we don't need it we don't use cookies, but in this case we need to let it enable cause we are using auth statefull
+                .csrf(Customizer.withDefaults())
+                //.authorizeHttpRequests(ar->ar.requestMatchers("/index/**").hasRole("USER"))
+                //.authorizeHttpRequests(ar->ar.requestMatchers("/save**/**","/delete/**").hasRole("ADMIN"))
+                //.authorizeHttpRequests(ar->ar.requestMatchers("/user/**").hasRole("USER"))
+                //.authorizeHttpRequests(ar->ar.requestMatchers("/admin/**").hasRole("ADMIN"))
+                .authorizeHttpRequests(ar->ar.requestMatchers("/public/**","/webjars/**").permitAll())
+                .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
+                .exceptionHandling(eh->eh.accessDeniedPage("/notAuthorized"))
+                .build();
+    }
+}
--- src/main/java/com/vogdo/springmvcthymeleafspringsecurity/web/ProductController.java
+++ src/main/java/com/vogdo/springmvcthymeleafspringsecurity/web/ProductController.java
@@ -2,10 +2,16 @@ package com.vogdo.springmvcthymeleafspringsecurity.web;
 
 import com.vogdo.springmvcthymeleafspringsecurity.entities.Product;
 import com.vogdo.springmvcthymeleafspringsecurity.repository.ProductRepository;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -14,15 +20,51 @@ import java.util.List;
 public class ProductController {
     @Autowired
     private ProductRepository productRepository;
-    @GetMapping("/index")
+    @GetMapping("/user/index")
+    @PreAuthorize("hasRole('USER')")
     public String index(Model model) {
         List<Product> products = productRepository.findAll();
         model.addAttribute("productsList", products);
         return "products";
     }
-    @GetMapping("/delete")
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/user/index";
+    }
+
+    @PostMapping("/admin/delete")
+    @PreAuthorize("hasRole('ADMIN')")
     public String delete(@RequestParam(name = "id") Long id){
         productRepository.deleteById(id);
-        return  "redirect:/index";
+        return  "redirect:/";
+    }
+
+    @GetMapping("/admin/newProduct")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String newProduct(Model model) {
+        model.addAttribute("product", new Product());
+        return "new-product";
+    }
+
+    @PostMapping("/admin/saveProduct")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String saveProduct(@Valid Product product, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) return "new-product";
+        productRepository.save(product);
+        return "redirect:/";
+    }
+    @GetMapping("/notAuthorized")
+    public String notAuthorized() {
+        return "notAuthorized";
+    }
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "login";
     }
 }
